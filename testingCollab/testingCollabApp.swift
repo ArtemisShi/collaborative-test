e351eac6452dbf7e1a1fe2d9dfbc422c4379f02c
BT_C09_15 <mobileappdev@bths.edu>
2026-04-23 13:49:06 -0400

    Initial Commit

---
+++ testingCollab/testingCollabApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingCollabApp.swift
+//  testingCollab
+//
+//  Created by Student on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct testingCollabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
