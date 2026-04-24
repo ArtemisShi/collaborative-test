256710961f181a1458cc2258784cd150711e7b0a
BT_C09_15 <mobileappdev@bths.edu>
2026-04-24 13:28:10 -0400

    Added line

--- testingCollab/ContentView.swift
+++ testingCollab/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Image(systemName: "square.and.arrow.up.badge.checkmark.fill")
+            Text("This is for testing")
         }
         .padding()
     }
