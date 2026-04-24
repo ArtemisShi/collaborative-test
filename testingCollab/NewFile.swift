8378430d8ff3341f653da81b0822650f8fc4cda9
BT_C09_12 <mobileappdev@bths.edu>
2026-04-24 13:28:06 -0400

    Added new file

---
+++ testingCollab/NewFile.swift
@@ -0,0 +1,18 @@
+//
+//  NewFile.swift
+//  testingCollab
+//
+//  Created by Student on 4/24/26.
+//
+
+import SwiftUI
+
+struct NewFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewFile()
+}
